30acebb0c2502ed2fdd942bd2baf9bd9ee78e573
alageev <alageev5610@gmail.com>
2021-02-05 16:59:53 +0200

    Add fetch request

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Header } from './components';
 import { Home, Cart, NotFound } from './pages';
 
-export const App = () => (
-  <div className='wrapper'>
-    <Header />
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/cart' component={Cart} />
-      <Route component={NotFound} />
-    </Switch>
-    <div className='content'></div>
-  </div>
-);
+export const App = () => {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    const fetchPizzasData = async () => {
+      try {
+        const response = await fetch(
+          'https://my-api-test-2021.herokuapp.com/pizzas'
+        );
+        const pizzasData = await response.json();
+        setPizzas(pizzasData);
+      } catch (err) {
+        console.log('error', err);
+      }
+    };
+    fetchPizzasData();
+  }, []);
+  return (
+    <div className='wrapper'>
+      <Header />
+      <div className='content'>
+        <Switch>
+          <Route exact path='/' render={() => <Home pizzas={pizzas} />} />
+          <Route exact path='/cart' component={Cart} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </div>
+  );
+};
